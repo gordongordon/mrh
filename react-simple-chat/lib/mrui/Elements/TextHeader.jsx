8106b096a873a2311186a65a6744493c27db0d68
gordongordon <3225297@gmail.com>
2018-01-12 17:46:30 +0800

    add MrCard, TextHeader

---
+++ react-simple-chat/lib/mrui/Elements/TextHeader.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+// import theme from './theme';
+import {Text} from 'rebass';
+
+const TextHeader= styled(Text)`
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    font-size : 1rem;
+`
+
+export default TextHeader;
